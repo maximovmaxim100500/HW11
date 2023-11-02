9c7fa76e269b00750756f0c998d432418d0eb12a
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-11-03 00:38:12 +0300

    домашка готова

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
             return false;
         }
     }
+
     public static void task1() {
         System.out.println("=========================   ");
         System.out.println("Задача 1");
@@ -20,6 +21,7 @@ public class Main {
             System.out.println(year + " год не високосный.");
         }
     }
+
     public static int сheckDevice(int OS, int year) { // проверка девайса
         int currentYear = LocalDate.now().getYear();
         if (OS == 0 && year < currentYear) {
@@ -34,27 +36,52 @@ public class Main {
             return 5;
         }
     }
+
     public static void task2() {
         System.out.println("=========================   ");
         System.out.println("Задача 2");
         int clientOS = 1;
         int clientDeviceYear = 2023;
-        if (сheckDevice(clientOS,clientDeviceYear) == 1) {
+        if (сheckDevice(clientOS, clientDeviceYear) == 1) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (сheckDevice(clientOS,clientDeviceYear) == 2) {
+        } else if (сheckDevice(clientOS, clientDeviceYear) == 2) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (сheckDevice(clientOS,clientDeviceYear) == 3) {
+        } else if (сheckDevice(clientOS, clientDeviceYear) == 3) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (сheckDevice(clientOS,clientDeviceYear) == 4) {
+        } else if (сheckDevice(clientOS, clientDeviceYear) == 4) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (сheckDevice(clientOS,clientDeviceYear) == 5){
+        } else if (сheckDevice(clientOS, clientDeviceYear) == 5) {
             System.out.println("Неизвестная операционная система.");
         }
     }
+
+    public static int deliveryTimeCalculation(int distance) { // Расчет времени доставки
+        int deliveryTime = 1;
+        if (distance <= 20) {
+            return deliveryTime;
+        } else if (distance > 20 && distance <= 60) {
+            deliveryTime++;
+            return deliveryTime;
+        } else if (distance > 60 && distance <= 100) {
+            deliveryTime += 2;
+            return deliveryTime;
+        } else {
+            return 4;
+        }
+
+    }
+
     public static void task3() {
         System.out.println("=========================   ");
         System.out.println("Задача 3");
+        int deliveryDistance = 111;
+        if (deliveryTimeCalculation(deliveryDistance) <= 3) {
+            System.out.println(" Потребуется дней: " + deliveryTimeCalculation(deliveryDistance));
+        } else {
+            System.out.println("Свыше 100км доставки нет.");
+        }
     }
+
     public static void main(String[] args) {
         task1();
         task2();
